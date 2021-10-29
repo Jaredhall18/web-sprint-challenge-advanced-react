a89869b1d63664fb736acc0728af250751de12c6
jaredhall18 <jaredhall18@gmail.com>
2021-10-29 11:23:12 -0600

    Added additional functionality to useForm Hook, moving successmessage state and onsubmit to the hook.

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -15,15 +15,8 @@ const initialValue = {
 // and replace the necessary stateful logic from CheckoutForm with the hook
 
 const CheckoutForm = (props) => {
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, handleChanges] = useForm(initialValue);
+  const [values, showSuccessMessage, handleSubmit, handleChanges] = useForm(initialValue);
 
- 
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setShowSuccessMessage(true);
-  };
 
   return (
     <>
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -2,13 +2,18 @@ import { useState } from 'react';
 
 const useForm = (initialValue) => {
     const [values, setValues] = useState(initialValue);
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
     const handleChanges = (e) => {
       setValues({ ...values, [e.target.name]: e.target.value });
     };
     
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setShowSuccessMessage(true);
+      };
     
-    return([values, handleChanges]);
+    return([values, showSuccessMessage, handleSubmit, handleChanges]);
 }
 
 export default useForm;
